94ccb9224bf7459859b47642fad868db997dacf1
Rakibul Islam Sarker <web.rakibul.3316@gmail.com>
2025-05-12 08:31:42 +0600

    🔨 Cleanup codebase.

--- src/pages/ProductManagement/ProductList.jsx
+++ src/pages/ProductManagement/ProductList.jsx
@@ -1,13 +1,3 @@
-import React, { useEffect } from "react";
-import { useGetAllClassRoomsMutation } from "../../redux/features/classRoom/classRoomApi";
-
 export default function ProductList() {
-  const [getAllClassRooms, { data }] = useGetAllClassRoomsMutation();
-
-  console.log("data >>", data);
-
-  useEffect(() => {
-    getAllClassRooms();
-  }, [getAllClassRooms]);
   return <div>ProductList data</div>;
 }
